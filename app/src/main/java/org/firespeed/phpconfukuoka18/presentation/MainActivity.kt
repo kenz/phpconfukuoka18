4f6b6e4bae39d6e00616f1073cd6da02095d20f0
kenz_firespeed <firespeedkenz@gmail.com>
2018-05-22 15:56:25 +0900

    change namespace.

--- app/src/main/java/org/firespeed/phpconfukuoka18/presentation/MainActivity.kt
+++ app/src/main/java/org/firespeed/phpconfukuoka18/presentation/MainActivity.kt
@@ -1,9 +1,10 @@
-package org.firespeed.phpconfukuoka18
+package org.firespeed.phpconfukuoka18.presentation
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import org.firespeed.phpconfukuoka18.R
 
 class MainActivity : AppCompatActivity() {
 
