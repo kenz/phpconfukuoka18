3ebaa3a743a161faa4c04b5c69172e7e98b8b4d0
kenz_firespeed <firespeedkenz@gmail.com>
2018-05-22 15:49:40 +0900

    change bottom navigation.

---
+++ app/src/main/kotlin/org/firspeed/phpconfukuoka18/MainActivity.kt
@@ -0,0 +1,20 @@
+package org.firespeed.phpconfukuoka18
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+       true
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+}
