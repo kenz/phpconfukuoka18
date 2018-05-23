72f7bd9edb7e9af1e3433d19a312b2b8a1be826b
kenz_firespeed <firespeedkenz@gmail.com>
2018-05-23 15:47:56 +0900

    Implemented Explorer Fragment.

--- app/src/main/java/org/firespeed/phpconfukuoka18/presentation/ExplorerFragment.kt
+++ app/src/main/java/org/firespeed/phpconfukuoka18/presentation/ExplorerFragment.kt
@@ -9,6 +9,10 @@ import android.view.View
 import android.view.ViewGroup
 import org.firespeed.phpconfukuoka18.R
 import org.firespeed.phpconfukuoka18.databinding.FragmentExplorerBinding
+import android.content.Intent
+import android.net.Uri
+import android.provider.CalendarContract
+import java.util.*
 
 
 class ExplorerFragment : Fragment() {
@@ -16,7 +20,7 @@ class ExplorerFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val binding:FragmentExplorerBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_explorer, container, false)
+        val binding: FragmentExplorerBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_explorer, container, false)
         binding.fragment = this
         listener?.setSupportActionBar(binding.toolbar)
         return binding.root
@@ -37,7 +41,57 @@ class ExplorerFragment : Fragment() {
         listener = null
     }
 
-    interface OnFragmentInteractionListener: ActivityInterface
+    @Suppress("UNUSED_PARAMETER")
+    fun addSchedule(view: View) {
+        val intent = Intent(Intent.ACTION_EDIT)
+        intent.data = CalendarContract.Events.CONTENT_URI
+        intent.type = "vnd.android.cursor.item/event"
+        intent.putExtra("title", getString(R.string.event_name))
+        intent.putExtra("eventLocation", getString(R.string.event_location)) // 場所
+        val begin = Calendar.getInstance()
+        begin.timeZone = TimeZone.getTimeZone("Asia/Tokyo")
+        begin.set(2018, 5, 16, 10, 0)
+        intent.putExtra("beginTime", begin.timeInMillis) // 開始日時
+        val end = Calendar.getInstance()
+        end.timeZone = TimeZone.getTimeZone("Asia/Tokyo")
+        end.set(2018, 5, 16, 18, 0)
+        intent.putExtra("endTime", end.timeInMillis) // 終了日時
+        intent.putExtra(CalendarContract.Events.AVAILABILITY, CalendarContract.Events.AVAILABILITY_BUSY)
+        intent.putExtra("allDay", false)
+        startActivity(Intent.createChooser(intent, getString(R.string.select_calender)))
+
+    }
+
+    @Suppress("UNUSED_PARAMETER")
+    fun startNavigation(view: View) {
+        val location = getString(R.string.location_data)
+        val gmmIntentUri = Uri.parse("geo:0,0?q=33.5912207,130.4149073($location)")
+        val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+        mapIntent.`package` = "com.google.android.apps.maps"
+        startActivity(mapIntent)
+
+    }
+
+    @Suppress("UNUSED_PARAMETER")
+    fun attendConference(view: View) {
+        val uri = Uri.parse("https://eventon.jp/11722/")
+        startActivity(Intent(Intent.ACTION_VIEW, uri))
+    }
+
+    @Suppress("UNUSED_PARAMETER")
+    fun attendParty(view: View) {
+        val uri = Uri.parse("https://eventon.jp/11973/")
+        startActivity(Intent(Intent.ACTION_VIEW, uri))
+    }
+
+    override fun startActivity(intent: Intent) {
+        val packageManager = context?.packageManager ?: return
+        if (intent.resolveActivity(packageManager) != null) {
+            super.startActivity(intent)
+        }
+    }
+
+    interface OnFragmentInteractionListener : ActivityInterface
 
     companion object {
         @JvmStatic
