fa2135115947bcb85c81a047d45e7134bc62fbaf
kenz_firespeed <firespeedkenz@gmail.com>
2018-05-23 12:01:37 +0900

    Introduce fragments tab.

---
+++ app/src/main/java/org/firespeed/phpconfukuoka18/presentation/AboutFragment.kt
@@ -0,0 +1,46 @@
+package org.firespeed.phpconfukuoka18.presentation
+
+import android.content.Context
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.firespeed.phpconfukuoka18.R
+import org.firespeed.phpconfukuoka18.databinding.FragmentAboutBinding
+
+
+class AboutFragment : Fragment() {
+    private var listener: OnFragmentInteractionListener? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding: FragmentAboutBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_about, container, false)
+        binding.fragment = this
+        listener?.setSupportActionBar(binding.toolbar)
+        return binding.root
+    }
+
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnFragmentInteractionListener) {
+            listener = context
+        } else {
+            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    interface OnFragmentInteractionListener : ActivityInterface
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = AboutFragment()
+    }
+}
--- app/src/main/java/org/firespeed/phpconfukuoka18/presentation/MainActivity.kt
+++ app/src/main/java/org/firespeed/phpconfukuoka18/presentation/MainActivity.kt
@@ -2,19 +2,30 @@ package org.firespeed.phpconfukuoka18.presentation
 
 import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.annotation.StringRes
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import org.firespeed.phpconfukuoka18.R
 import org.firespeed.phpconfukuoka18.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), ExplorerFragment.OnFragmentInteractionListener {
+class MainActivity : AppCompatActivity(),
+        ExplorerFragment.OnFragmentInteractionListener,
+        TimeTableFragment.OnFragmentInteractionListener,
+        FavoriteFragment.OnFragmentInteractionListener,
+        AboutFragment.OnFragmentInteractionListener {
 
     private lateinit var binding: ActivityMainBinding
     private var explorerFragment: ExplorerFragment? = null
+    private var timeTableFragment: TimeTableFragment? = null
+    private var favoriteFragment: FavoriteFragment? = null
+    private var aboutFragment: AboutFragment? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         explorerFragment = supportFragmentManager.findFragmentByTag(TAG_EXPLORER) as ExplorerFragment?
+        timeTableFragment= supportFragmentManager.findFragmentByTag(TAG_TIME_TABLE) as TimeTableFragment?
+        favoriteFragment = supportFragmentManager.findFragmentByTag(TAG_FAVORITE) as FavoriteFragment?
+        aboutFragment = supportFragmentManager.findFragmentByTag(TAG_ABOUT) as AboutFragment?
 
         binding.activity = this
         binding.navigation.setOnNavigationItemSelectedListener { setFragment(it.itemId) }
@@ -29,17 +40,22 @@ class MainActivity : AppCompatActivity(), ExplorerFragment.OnFragmentInteraction
     private fun setFragment(itemId: Int): Boolean =
             when (itemId) {
                 R.id.navigation_explorer ->
-                    setMainFragment(explorerFragment
-                            ?: ExplorerFragment.newInstance(), TAG_EXPLORER)
-
+                    setMainFragment(explorerFragment ?: ExplorerFragment.newInstance(), TAG_EXPLORER, R.string.title_explorer)
+                R.id.navigation_time_table->
+                    setMainFragment(timeTableFragment?: TimeTableFragment.newInstance(), TAG_TIME_TABLE, R.string.title_time_table)
+                R.id.navigation_favorite->
+                    setMainFragment(favoriteFragment?: FavoriteFragment.newInstance(), TAG_FAVORITE, R.string.title_favorite)
+                R.id.navigation_about->
+                    setMainFragment(aboutFragment?: TimeTableFragment.newInstance(), TAG_ABOUT, R.string.title_about)
                 else -> {
-                    true
+                    false
                 }
 
             }
 
-    private fun setMainFragment(fragment: Fragment, tag: String): Boolean {
+    private fun setMainFragment(fragment: Fragment, tag: String,@StringRes titleId:Int): Boolean {
         supportFragmentManager.beginTransaction().replace(binding.mainFragment.id, fragment, tag).commit()
+        setTitle(titleId)
         return true
     }
 
@@ -47,5 +63,8 @@ class MainActivity : AppCompatActivity(), ExplorerFragment.OnFragmentInteraction
 
         private const val STATE_MENU = "menu"
         private const val TAG_EXPLORER = "explorer"
+        private const val TAG_TIME_TABLE= "timeTable"
+        private const val TAG_FAVORITE = "favorite"
+        private const val TAG_ABOUT= "about"
     }
 }
