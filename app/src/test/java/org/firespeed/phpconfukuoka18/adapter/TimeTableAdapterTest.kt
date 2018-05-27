0470d71a61105832e89f022df57a9a3aedcb4e50
kenz_firespeed <firespeedkenz@gmail.com>
2018-05-28 00:48:56 +0900

    Fix test pattern.

--- app/src/test/java/org/firespeed/phpconfukuoka18/adapter/TimeTableAdapterTest.kt
+++ app/src/test/java/org/firespeed/phpconfukuoka18/adapter/TimeTableAdapterTest.kt
@@ -2,7 +2,6 @@ package org.firespeed.phpconfukuoka18.adapter
 
 import org.firespeed.phpconfukuoka18.model.Session
 import org.junit.Assert
-import org.junit.Assert.*
 import org.junit.Test
 
 class TimeTableAdapterTest {
@@ -10,8 +9,8 @@ class TimeTableAdapterTest {
     fun setItemTest() {
         val adapter = TimeTableAdapter()
         val list =ArrayList<Session>()
-        list.add(Session(1L,"location","11:11",1,"title",null,null,null,null,false))
-        list.add(Session(2L,"location","11:11",1,"title",null,null,null,null,false))
+        list.add(Session(1, "location", "11:11", 1, "title", null, null, null, null, false))
+        list.add(Session(2, "location", "11:11", 1, "title", null, null, null, null, false))
         adapter.setItem(list)
         Assert.assertEquals(adapter.itemCount,3)
         Assert.assertEquals(adapter.getItemViewType(0),TimeTableAdapter.VIEW_TYPE_TIME)
@@ -20,8 +19,8 @@ class TimeTableAdapterTest {
         adapter.setItem(list)
         Assert.assertEquals(adapter.itemCount,3)
 
-        list.add(Session(3L,"location3","12:12",2,"title",null,null,null,null,false))
-        list.add(Session(4L,"location4","12:12",2,"title",null,null,null,null,false))
+        list.add(Session(3, "location3", "12:12", 2, "title", null, null, null, null, false))
+        list.add(Session(4, "location4", "12:12", 2, "title", null, null, null, null, false))
 
         adapter.setItem(list)
         Assert.assertEquals(adapter.itemCount,6)
